647f53348219673e4341831169cdd477537995c1
AnderoV <andero@skenifor.ee>
2025-05-17 11:15:56 +0300

    vc

---
+++ app/Services/TaskService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Task;
+use Illuminate\Support\Facades\Auth;
+
+class TaskService
+{
+    
+     // tagastab kõik kasutaja ülesanded 
+    public function all()
+    {
+        return Auth::user()->tasks;
+    }
+
+    public function create(array $data)
+    {
+        return Auth::user()->tasks()->create($data);
+    }
+
+    public function update(Task $task, array $data)
+    {
+        return $task->update($data);
+    }
+
+    public function delete(Task $task)
+    {
+        return $task->delete();
+    }
+}
